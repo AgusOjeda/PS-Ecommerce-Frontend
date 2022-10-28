8235579d430ecc5cb80e57b984784ebb1dd653d8
Agustin Ojeda <64862757+AgusOjeda@users.noreply.github.com>
2022-10-28 07:29:06 -0300

    configuration dependencies and express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+const router = express.Router()
+const reload = require('reload')
+
+// Path: routes/index.js
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/view/index.html')))
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/', router)
+app.listen(port, () => console.log(`listening on port ${port}!`))
+reload(app)
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './public/view/*.{html,js}',
+    './public/js/components/*.js',
+    './public/js/container/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
