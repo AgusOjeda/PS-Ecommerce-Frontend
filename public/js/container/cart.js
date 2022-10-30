ab07c9a052e0a10a07b03a8b8e760470a725b264
Agustin Ojeda <64862757+AgusOjeda@users.noreply.github.com>
2022-10-30 20:25:21 -0300

    some changes

--- public/js/container/cart.js
+++ public/js/container/cart.js
@@ -4,17 +4,30 @@ let _root
 export const CartRender = async () => {
   _root = document.getElementById('root')
   _root.innerHTML = /* html */'<h1>Cart</h1>'
+}
+const renderCart = () => {
   const cart = getCartOnLocalStorage()
   console.log(cart)
   cart.forEach(element => {
     _root.innerHTML += /* html */`
-      <div>
-        <h3>${element.productId}</h3>
+    <h1>CARRITO DE PRODUCTOS</h1>
+      <div class="grid justify-items-center grid-cols-4>
+        <img src="" alt"" />
         <p>${element.amount}</p>
       </div>
     `
   })
 }
+const frame = () => {
+
+}
+const CreateCartUI = (product) => {
+  const { id, nombre, marca, precio, descripcion, imagenUrl } = product
+
+  saveCartOnLocalStorage(cart)
+  renderCart()
+}
+
 /*
 const RenderProductPage = async () => {
   const { view, title } = renderFrame()
