98f97669b5f1728a6f589cfdb7825dc9817f9aa1
Agustin Ojeda <64862757+AgusOjeda@users.noreply.github.com>
2022-11-10 15:22:07 -0300

    ADD New Modal

--- public/js/services/modal.js
+++ public/js/services/modal.js
@@ -1,5 +1,6 @@
 import { ProductById } from './ProductService.js'
 import { ModalProduct } from '../components/modalProduct.js'
+import { OftenWithCard } from '../components/oftenWith.js'
 
 export const modal = () => {
   const products = document.getElementsByClassName('modal-open')
@@ -22,6 +23,7 @@ const createModal = (body) => {
   const modal = ModalProduct(body)
   const modalContainer = document.createElement('div')
   modalContainer.setAttribute('id', 'modal-container')
+  modalContainer.classList.add('fixed', 'inset-0', 'bg-gray-700', 'bg-opacity-75', 'font-GilroyRegular')
   modalContainer.innerHTML = modal
   document.body.append(modalContainer)
   const plus = document.querySelector('#quantity-increment')
@@ -38,6 +40,7 @@ const createModal = (body) => {
       num.innerHTML = count
     }
   })
+  load()
 }
 
 const deleteModal = () => {
@@ -45,5 +48,41 @@ const deleteModal = () => {
   const modal = document.getElementById('modal-container')
   closeButton.onclick = () => {
     document.body.removeChild(modal)
+  }
+}
+
+const load = async () => {
+  for (let i = 0; i < 5; i++) {
+    generateId(5)
+  }
+  haveIt.forEach((id) => {
+    ProductById(id, (body) => {
+      RenderOftenProduct(body)
+    })
+  })
+}
+
+const RenderOftenProduct = async (product) => {
+  const oftenList = document.getElementById('product-list-bought')
+  const productBox = document.createElement('div')
+  productBox.classList.add('rounded-2xl', 'p-2', 'text-textColor')
+  productBox.setAttribute('id', product.id)
+
+  const productContent = OftenWithCard(product)
+  productBox.innerHTML = productContent
+  oftenList.append(productBox)
+}
+const haveIt = []
+
+function generateId (maxNr) {
+  const random = Math.ceil(Math.random() * maxNr)
+  if (!haveIt.includes(random)) {
+    haveIt.push(random)
+  } else {
+    if (haveIt.length < maxNr) {
+      return generateId(maxNr)
+    } else {
+      return false
+    }
   }
 }
